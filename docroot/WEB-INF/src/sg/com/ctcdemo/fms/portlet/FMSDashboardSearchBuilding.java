3553fb18a899a283c3b9ea583cf091c5cfb36a1c
fernandokarnagi <fernando.karnagi@ctc-g.com.sg>
2016-01-03 14:59:16 +0800

    added Dashbaord Map search building portlet

---
+++ docroot/WEB-INF/src/sg/com/ctcdemo/fms/portlet/FMSDashboardSearchBuilding.java
@@ -0,0 +1,11 @@
+package sg.com.ctcdemo.fms.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class FMSDashboardSearchBuilding
+ */
+public class FMSDashboardSearchBuilding extends MVCPortlet {
+ 
+
+}
