eae2442ecb280bf41b94fcf5be6940bf8cbc667f
fernandokarnagi <fernando.karnagi@ctc-g.com.sg>
2016-01-03 12:33:58 +0800

    first commit

---
+++ docroot/WEB-INF/src/sg/com/ctcdemo/fms/portlet/FMSHomePortlet.java
@@ -0,0 +1,11 @@
+package sg.com.ctcdemo.fms.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class FMSHomePortlet
+ */
+public class FMSHomePortlet extends MVCPortlet {
+ 
+
+}
