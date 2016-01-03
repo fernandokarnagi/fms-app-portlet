ba2080ffe26eaafac2320b0b6cc1fb388adec8fe
fernandokarnagi <fernando.karnagi@ctc-g.com.sg>
2016-01-03 14:53:48 +0800

    added Dashboard Map portlet

---
+++ docroot/WEB-INF/src/sg/com/ctcdemo/fms/portlet/FMSDashboardMap.java
@@ -0,0 +1,11 @@
+package sg.com.ctcdemo.fms.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class FMSDashboardMap
+ */
+public class FMSDashboardMap extends MVCPortlet {
+ 
+	
+}
