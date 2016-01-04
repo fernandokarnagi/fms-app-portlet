19d9d8c4140cb22ab66b11b8eba5aedf3c5eeef9
fernandokarnagi <fernando.karnagi@ctc-g.com.sg>
2016-01-04 13:15:12 +0800

    added supermarkeet PM Chart

---
+++ docroot/WEB-INF/src/sg/com/ctcdemo/fms/portlet/FMSSupermarketPMChart.java
@@ -0,0 +1,11 @@
+package sg.com.ctcdemo.fms.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class FMSSupermarketPMChart
+ */
+public class FMSSupermarketPMChart extends MVCPortlet {
+ 
+
+}
