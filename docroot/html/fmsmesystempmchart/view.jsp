8ce6923ec1a24a3f8740a8ade5fcfa70687e60eb
fernandokarnagi <fernando.karnagi@ctc-g.com.sg>
2016-01-05 11:41:38 +0800

    updated PM charts

--- docroot/html/fmsmesystempmchart/view.jsp
+++ docroot/html/fmsmesystempmchart/view.jsp
@@ -1,5 +1,67 @@
 <%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
 
 <portlet:defineObjects />
+<script src="<%=request.getContextPath()%>/js/Chart.min.js"></script>
 
-This is the <b>FMS ME System Pm Chart</b> portlet in View mode.
+<form class="form-inline">
+  <fieldset>
+    <label>Building&nbsp;&nbsp;</label>
+    <label class="checkbox">
+      <input type="checkbox" checked>One Marina Boulevard
+    </label>
+     <label class="checkbox">
+      <input type="checkbox" checked>One Shenton Way
+    </label>
+     <label class="checkbox">
+      <input type="checkbox" checked>One Raffles Place
+    </label>
+   
+    <label>&nbsp;&nbsp;Month</label>
+    <label class="checkbox">
+      <select>
+      	<option>January</option>
+      	<option>February</option>
+      	<option>March</option>
+      	<option>April</option>
+      	<option>May</option>
+      	<option>June</option>
+      	<option>July</option>
+      	<option>August</option>
+      	<option>September</option>
+      	<option>October</option>
+      	<option>November</option>
+      	<option>December</option>
+      </select>
+    </label>
+    
+    <label>&nbsp;&nbsp;Year</label>
+    <label class="checkbox">
+      <select>
+      	<option>2016</option>
+      	<option>2015</option>
+      	<option>2014</option>
+      </select>
+    </label>
+    
+    <button >Refresh</button>
+  </fieldset>
+  
+  
+</form>
+
+<hr/>
+
+<div id="fmsCompleted" class="fms-pie-chart"><b>On Schedule and Completed</b><canvas id="fmsCompletedChart" width="200px" height="200px"></canvas></div>
+<div id="fmsNotOnSchedule" class="fms-pie-chart"><b>Not On Schedule and Completed</b><canvas id="fmsNotOnScheduleChart" width="200px" height="200px"></canvas></div>
+<div id="fmsNotComplete" class="fms-pie-chart"><b>Not Completed</b><canvas id="fmsNotCompletedChart" width="200px" height="200px"></canvas></div>
+<div id="fmsTotal" class="fms-pie-chart"><b>Total</b><canvas id="fmsTotalChart" width="200px" height="200px"></canvas></div>
+
+<br class="fms-clearBoth" />
+<br class="fms-clearBoth" />
+
+    <button class="btn icon-print">Print</button>
+    <button class="btn icon-download">Export</button>
+    <br class="fms-clearBoth" />
+    <br class="fms-clearBoth" />
+    
+<div id="pmTable" class="table table-bordered table-hover"></div>
---
+++ docroot/js/security-system-pm-chart.js
@@ -0,0 +1,300 @@
+YUI()
+		.use(
+				'aui-datatable',
+				function(Y) {
+					
+					var columnSet = [ {
+						key : "0",
+						allowHTML : true,
+						label : "Resources",
+						formatter : '{value}'
+					} ];
+					for (var i = 1; i <= 31; i++) {
+						columnSet
+								.push({
+									key : "" + i,
+									label : "" + i,
+									allowHTML : true,
+									formatter : '<img src="/fms-app-portlet/images/circle-{value}.png" width="15px" height="15px"/>'
+								});
+					}
+
+					var data = [ {
+						0 : 'Watchman Tour Route',
+						1 : 'green',
+						2 : 'red',
+						3 : 'green',
+						4 : 'green',
+						5 : 'green',
+						6 : 'yellow',
+						7 : 'green',
+						8 : 'green',
+						9 : 'green',
+						10 : 'green',
+						11 : 'green',
+						12 : 'green',
+						13 : 'green',
+						14 : 'green',
+						15 : 'green',
+						16 : 'green',
+						17 : 'green',
+						18 : 'green',
+						19 : 'green',
+						20 : 'green',
+						21 : 'green',
+						22 : 'green',
+						23 : 'yellow',
+						24 : 'green',
+						25 : 'green',
+						26 : 'green',
+						27 : 'green',
+						28 : 'green',
+						29 : 'green',
+						30 : 'green',
+						31 : 'green'
+					}, {
+						0 : 'CCTV System',
+						1 : 'green',
+						2 : 'red',
+						3 : 'green',
+						4 : 'green',
+						5 : 'green',
+						6 : 'green',
+						7 : 'green',
+						8 : 'green',
+						9 : 'green',
+						10 : 'green',
+						11 : 'green',
+						12 : 'green',
+						13 : 'green',
+						14 : 'green',
+						15 : 'green',
+						16 : 'green',
+						17 : 'green',
+						18 : 'green',
+						19 : 'green',
+						20 : 'green',
+						21 : 'green',
+						22 : 'yellow',
+						23 : 'green',
+						24 : 'green',
+						25 : 'green',
+						26 : 'green',
+						27 : 'green',
+						28 : 'green',
+						29 : 'green',
+						30 : 'green',
+						31 : 'green'
+					}, {
+						0 : 'Card Access System',
+						1 : 'green',
+						2 : 'red',
+						3 : 'green',
+						4 : 'green',
+						5 : 'green',
+						6 : 'green',
+						7 : 'green',
+						8 : 'green',
+						9 : 'green',
+						10 : 'green',
+						11 : 'green',
+						12 : 'green',
+						13 : 'green',
+						14 : 'green',
+						15 : 'green',
+						16 : 'red',
+						17 : 'green',
+						18 : 'green',
+						19 : 'green',
+						20 : 'green',
+						21 : 'green',
+						22 : 'green',
+						23 : 'green',
+						24 : 'green',
+						25 : 'yellow',
+						26 : 'green',
+						27 : 'green',
+						28 : 'green',
+						29 : 'green',
+						30 : 'green',
+						31 : 'green'
+					}, {
+						0 : 'Turnstile System',
+						1 : 'green',
+						2 : 'red',
+						3 : 'green',
+						4 : 'green',
+						5 : 'green',
+						6 : 'green',
+						7 : 'green',
+						8 : 'green',
+						9 : 'green',
+						10 : 'green',
+						11 : 'green',
+						12 : 'green',
+						13 : 'green',
+						14 : 'green',
+						15 : 'green',
+						16 : 'red',
+						17 : 'green',
+						18 : 'green',
+						19 : 'green',
+						20 : 'green',
+						21 : 'green',
+						22 : 'green',
+						23 : 'green',
+						24 : 'green',
+						25 : 'yellow',
+						26 : 'green',
+						27 : 'green',
+						28 : 'green',
+						29 : 'green',
+						30 : 'green',
+						31 : 'green'
+					}, {
+						0 : 'Watchman Tour System',
+						1 : 'green',
+						2 : 'red',
+						3 : 'green',
+						4 : 'green',
+						5 : 'green',
+						6 : 'green',
+						7 : 'green',
+						8 : 'green',
+						9 : 'green',
+						10 : 'green',
+						11 : 'green',
+						12 : 'green',
+						13 : 'green',
+						14 : 'green',
+						15 : 'green',
+						16 : 'red',
+						17 : 'green',
+						18 : 'green',
+						19 : 'green',
+						20 : 'green',
+						21 : 'green',
+						22 : 'green',
+						23 : 'green',
+						24 : 'green',
+						25 : 'yellow',
+						26 : 'green',
+						27 : 'green',
+						28 : 'green',
+						29 : 'green',
+						30 : 'green',
+						31 : 'green'
+					} ];
+
+					new Y.DataTable({
+						columns : columnSet,
+						width : '100%',
+						height : '100%',
+						data : data
+					}).render('#pmTable');
+				});
+
+var notCompletedData  = [
+    {
+        value: 5,
+        color:"#ff0000",
+        highlight: "#dd0000",
+        label: "Not Completed"
+    },
+    {
+        value: 95,
+        color: "#dddddd",
+        highlight: "#aaaaaa",
+        label: "Others"
+    }
+]
+
+var notOnScheduleData  = [
+    {
+        value: 15,
+        color:"#ff9900",
+        highlight: "#dddd00",
+        label: "Not On Schedule"
+    },
+    {
+        value: 85,
+        color: "#dddddd",
+        highlight: "#aaaaaa",
+        label: "Others"
+    }
+]
+
+var completedData  = [
+    {
+        value: 80,
+        color:"#00ff00",
+        highlight: "#00dd00",
+        label: "Completed"
+    },
+    {
+        value: 20,
+        color: "#dddddd",
+        highlight: "#aaaaaa",
+        label: "Others"
+    }
+]
+
+var totalData  = [
+{
+    value: 5,
+    color:"#ff0000",
+    highlight: "#dd0000",
+    label: "Not Completed"
+},{
+    value: 15,
+    color:"#ff9900", 
+    highlight: "#dddd00",
+    label: "Not On Schedule & Completed" 
+},{
+    value: 80,
+    color:"#00ff00",
+    highlight: "#00dd00",
+    label: "On Schedule & Completed"
+}
+]
+
+var options = 
+{
+    //Boolean - Whether we should show a stroke on each segment
+    segmentShowStroke : true,
+
+    //String - The colour of each segment stroke
+    segmentStrokeColor : "#fff",
+
+    //Number - The width of each segment stroke
+    segmentStrokeWidth : 2,
+
+    //Number - The percentage of the chart that we cut out of the middle
+    percentageInnerCutout : 50, // This is 0 for Pie charts
+
+    //Number - Amount of animation steps
+    animationSteps : 100,
+
+    //String - Animation easing effect
+    animationEasing : "easeOutBounce",
+
+    //Boolean - Whether we animate the rotation of the Doughnut
+    animateRotate : true,
+
+    //Boolean - Whether we animate scaling the Doughnut from the centre
+    animateScale : true,
+
+    
+}
+
+var notCompletedCtx = document.getElementById("fmsNotCompletedChart").getContext("2d");
+new Chart(notCompletedCtx).Pie(notCompletedData,options);
+
+var notOnScheduleCtx = document.getElementById("fmsNotOnScheduleChart").getContext("2d");
+new Chart(notOnScheduleCtx).Pie(notOnScheduleData,options);
+
+var completedCtx = document.getElementById("fmsCompletedChart").getContext("2d");
+new Chart(completedCtx).Pie(completedData,options);
+
+var totalCtx = document.getElementById("fmsTotalChart").getContext("2d");
+new Chart(totalCtx).Pie(totalData,options);
